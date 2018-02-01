d5bb781889d80e758ebb9a9ca73edf05f8c35b0f
fodmar <marcinfoder@gmail.com>
2018-02-01 19:40:39 +0100

    probability grid only one checkbox checked in row

--- js/questionViewModel.js
+++ js/questionViewModel.js
@@ -4,6 +4,20 @@ define(["ko"], function (ko) {
         this.id = question.id;
         this.content = ko.observable(question.content);
         this.answerId = ko.observable(question.answerId);
+        
+        this.selectedAnswers = ko.observableArray([this.answerId()]);
+        
+        this.setAnswerId = function (answer, event) {
+            if (event.target.checked) {
+                this.answerId(answer.id);
+                this.selectedAnswers([answer.id]);
+            } else {
+                this.answerId(0);
+                this.selectedAnswers.removeAll();
+            }
+            
+            return true;
+        };
     };
    
     return QuestionViewModel;
